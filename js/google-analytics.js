2a099e7fb3db2bf306ce6597945764bb2a98f91b
Svenstar74 <sven.firmbach@gmx.de>
2023-12-13 21:26:48 +0100

    add scripts

---
+++ js/google-analytics.js
@@ -0,0 +1,6 @@
+// Legacy code, I don't really know what it does exactly
+window.dataLayer = window.dataLayer || [];
+function gtag(){dataLayer.push(arguments);}
+gtag('js', new Date());
+
+gtag('config', 'UA-165706180-1');
